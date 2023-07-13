992390edfc5126faf77b7f4f8487d0dd1fc65d23
Maxmajonk <maxmajonk@gmail.com>
2023-07-14 00:53:51 +0200

    still working

---
+++ ImageFeed/ImagineList/ImagesListCell.swift
@@ -0,0 +1,15 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Mikhail Vostrikov on 08.07.2023.
+//
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+
+    static let reuseIdentifier = "ImagesListCell"
+    
+    
+}
