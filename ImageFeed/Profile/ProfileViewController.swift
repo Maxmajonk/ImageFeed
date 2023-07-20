b38d9c6bf48f426f89ec96241aad76ed4a2f3217
Maxmajonk <maxmajonk@gmail.com>
2023-07-20 11:55:14 +0200

    Second Screen

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Mikhail Vostrikov on 20.07.2023.
+//
+
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    @IBOutlet private var avatarImageView: UIImageView!
+    @IBOutlet private var nameLabel: UILabel!
+    @IBOutlet private var loginNameLabel: UILabel!
+    @IBOutlet private var descriptionLabel: UILabel!
+
+    @IBOutlet private var logoutButton: UIButton!
+
+    @IBAction private func didTapLogoutButton() {
+    }
+}
+
