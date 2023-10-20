64eec1d66649c301115ac902241e9fba19b68778
Maxmajonk <maxmajonk@gmail.com>
2023-10-21 01:29:12 +0200

    Web +

---
+++ ImageFeed/Auth/AuthViewController.swift
@@ -0,0 +1,12 @@
+//
+//  AuthViewController.swift
+//  ImageFeed
+//
+//  Created by Mikhail Vostrikov on 20.10.2023.
+//
+
+import UIKit
+
+final class AuthViewController : UIViewController {
+    
+}
